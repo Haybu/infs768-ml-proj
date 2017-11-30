bbce0ca95b33517094ad225639ed7cd5d69e25e1
hxm3459 <haytham_mohamed@homedepot.com>
2017-11-30 11:50:43 -0600

    final set

---
+++ neural-network.py
@@ -0,0 +1,39 @@
+# %%
+from sklearn.neural_network import MLPClassifier
+from utils import commons
+
+# %%
+def classifier_config():
+    param_grid = {
+        'clf__learning_rate': ["constant", "invscaling", "adaptive"],
+        'clf__hidden_layer_sizes': [(50,50,10),(128,128,10),(150,150,10)],
+        'clf__alpha': [0.01, 0.001, 0.0001],
+        'clf__activation': ["logistic", "relu", "tanh"]
+        }
+
+    return {
+         'classifier': MLPClassifier(),
+         'parameters': param_grid
+      }
+
+# %%
+def run():
+    data = commons.digits_data() # commons.digits_data_slim()
+    x_train = data['train']['x'] / 255.
+    y_train = data['train']['y']
+    x_test = data['test']['x'] / 255.
+    y_test = data['test']['y']
+
+    config = classifier_config()
+    selector = commons.feature_selector_chi2(90)
+    model = commons.perform_grid_search(config['classifier']
+                              , config['parameters']
+                              , selector
+                              , x_train, y_train)
+    commons.score(model, x_test, y_test)
+
+# %%
+print("====  Start =====")
+reload(commons)
+run()
+print("====  End =====")
